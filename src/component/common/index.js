35c4d4c37c257c2ac5c4fc25bff3f6a419796b5b
phamviet86 <phamquangviet@bingoenglish.edu.vn>
2025-06-21 16:39:58 +0700

    add root page

---
+++ src/component/common/index.js
@@ -0,0 +1,18 @@
+// path: @/src/component/common/index.js
+
+export * from "./config-provider";
+// export * from "./loading";
+// export * from "./pro-layout";
+// export * from "./page-container";
+// export * from "./button";
+// export * from "./card";
+// export * from "./pro-table";
+// export * from "./pro-descriptions";
+// export * from "./drawer-info";
+// export * from "./drawer-form";
+// export * from "./result";
+// export * from "./pop-confirm";
+// export * from "./transfer";
+// export * from "./modal";
+// export * from "./modal-steps";
+// export * from "./full-calendar";
